a9c58137d96bce81e0e4f15fb8e4d7e45c5eeb4d
asahinadev <dev.account.01@mail.mirageworld.jp>
2019-04-11 16:39:27 +0900

    Yahoo! Japan Oauth2 認証 Default

---
+++ src/main/java/com/example/spring/yconnect/config/SecurityConfiguig.java
@@ -0,0 +1,76 @@
+package com.example.spring.yconnect.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguig
+		extends WebSecurityConfigurerAdapter {
+
+	@Override
+	public void configure(WebSecurity web)
+			throws Exception {
+		super.configure(web);
+
+		web.ignoring().antMatchers(
+				// webjars
+				"/webjars/**",
+				// CSS ファイル
+				"/css/**",
+				// JavaScriptファイル
+				"/js/**",
+				// 画像ファイル
+				"/img/**",
+				// サウンドファイル
+				"/sound/**",
+				// WEB フォント
+				"/font/**",
+				"/fonts/**",
+				// 外部ライブラリ
+				"/exlib/**"
+		/**/
+		);
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth)
+			throws Exception {
+		super.configure(auth);
+	}
+
+	@Override
+	protected void configure(HttpSecurity http)
+			throws Exception {
+		super.configure(http);
+
+		http.formLogin().disable();
+		http.logout().disable();
+
+		http.httpBasic().disable();
+
+		http.csrf().disable();
+
+		http.oauth2Login()
+
+				// 認証エンドポイント
+				.authorizationEndpoint()
+				.and()
+
+				// リダイレクトエンドポイント
+				.redirectionEndpoint()
+				.and()
+
+				// ユーザー情報エンドポイント
+				.userInfoEndpoint()
+				.and()
+
+		;
+
+	}
+
+}
