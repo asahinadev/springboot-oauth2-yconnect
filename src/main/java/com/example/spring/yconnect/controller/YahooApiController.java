1fcff44603317456b74a2cba3e91ec5693b25cfc
asahinadev <dev.account.01@mail.mirageworld.jp>
2020-05-31 12:28:36 +0900

    API 修正
    ローカルサーチ

--- src/main/java/com/example/spring/yconnect/controller/YahooApiController.java
+++ src/main/java/com/example/spring/yconnect/controller/YahooApiController.java
@@ -1,22 +1,19 @@
 package com.example.spring.yconnect.controller;
 
-import java.util.Arrays;
 import java.util.Map;
 import java.util.function.Consumer;
 
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import com.example.spring.yconnect.config.YahooApiConfig;
 import com.example.spring.yconnect.dto.GeoCoder;
+import com.example.spring.yconnect.dto.LocalSearch;
 import com.example.spring.yconnect.dto.ReverseGeoCoder;
 import com.example.spring.yconnect.dto.ydt.Ydf;
 
@@ -43,13 +40,12 @@ public class YahooApiController {
 
 	@ResponseBody
 	@GetMapping("local-search")
-	public Mono<Ydf> localSearch(@RequestParam MultiValueMap<String, String> params) {
-
-		MultiValueMap<String, String> _params = new LinkedMultiValueMap<>(params);
-		_params.put("output", Arrays.asList("json"));
-
+	public Mono<Ydf> localSearch(LocalSearch params) {
+		log.debug("{}", params);
 		return yahooapis.get()
-				.uri(b -> b.path("search/local/V1/localSearch").queryParams(_params).build()).retrieve()
+				.uri(b -> b.path("search/local/V1/localSearch")
+						.queryParams(params.parameters()).build())
+				.retrieve()
 				.bodyToMono(Ydf.class);
 	}
 
---
+++ src/main/java/com/example/spring/yconnect/dto/LocalSearch.java
@@ -0,0 +1,142 @@
+package com.example.spring.yconnect.dto;
+
+import java.util.Objects;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+import com.example.spring.yconnect.dto.ydt.Datum;
+
+import lombok.Data;
+
+@Data
+public class LocalSearch {
+
+	enum Sort {
+		string,
+		rating,
+		score,
+		hybrid,
+		review,
+		kana,
+		price,
+		dist,
+		geo,
+		match;
+
+		@Override
+		public String toString() {
+			return name();
+		}
+	}
+
+	String device;
+	String query;
+	String id;
+	String cid;
+	String uid;
+	String gid;
+	String bid;
+	String group;
+	boolean distinct = true;
+	Double lat;
+	Double lon;
+	String bbox;
+	Double dist;
+	Datum datum = Datum.tky;
+	String ac;
+	String gc;
+	boolean coupon = false;
+	boolean parking = false;
+	boolean creditcard = false;
+	String smoking;
+	boolean reservation = false;
+	boolean image = false;
+	String open;
+	boolean locoMode = true;
+	Long minprice;
+	Long maxprice;
+	Sort sort = Sort.score;
+	int page = 1;
+	int results = 10;
+
+	public MultiValueMap<String, String> parameters() {
+
+		MultiValueMap<String, String> m = new LinkedMultiValueMap<>();
+
+		if (StringUtils.equals(device, "mobile")) {
+			m.add("device", device);
+		}
+
+		if (StringUtils.isNotEmpty(id)) {
+			m.add("id", id);
+		}
+		if (StringUtils.isNotEmpty(cid)) {
+			m.add("cid", cid);
+		}
+		if (StringUtils.isNotEmpty(gid)) {
+			m.add("gid", gid);
+		}
+		if (StringUtils.isNotEmpty(uid)) {
+			m.add("uid", uid);
+		}
+		if (StringUtils.isNotEmpty(bid)) {
+			m.add("bid", gid);
+		}
+		if (StringUtils.isNotEmpty(group)) {
+			m.add("group", group);
+		}
+		m.add("distinct", Boolean.toString(distinct));
+		if (StringUtils.isNotEmpty(query)) {
+			m.add("query", query);
+			m.add("ei", "UTF-8");
+			m.add("ac", ac);
+		}
+		if (!Objects.isNull(lat) && !Objects.isNull(lon)) {
+			m.add("lat", Double.toString(lat));
+			m.add("lon", Double.toString(lon));
+			m.add("datum", datum.name());
+			if (!Objects.isNull(dist)) {
+				m.add("dist", Double.toString(dist));
+			}
+		} else if (StringUtils.isNotEmpty(bbox)) {
+			m.add("bbox", bbox);
+			m.add("datum", datum.name());
+		}
+		if (StringUtils.isNotEmpty(ac)) {
+			m.add("ac", ac);
+		}
+		if (StringUtils.isNotEmpty(gc)) {
+			m.add("gc", gc);
+		}
+		m.add("coupon", Boolean.toString(coupon));
+		m.add("parking", Boolean.toString(parking));
+		m.add("creditcard", Boolean.toString(creditcard));
+		if (StringUtils.isNotEmpty(smoking)) {
+			m.add("smoking", smoking);
+		}
+		m.add("reservation", Boolean.toString(reservation));
+		m.add("image", Boolean.toString(image));
+		if (StringUtils.isNotEmpty(open)) {
+			m.add("open", open);
+		}
+		m.add("loco_mode", String.valueOf(locoMode));
+		if (!Objects.isNull(minprice)) {
+			m.add("minprice", String.valueOf(minprice));
+		}
+		if (!Objects.isNull(maxprice)) {
+			m.add("maxprice", String.valueOf(maxprice));
+		}
+
+		m.add("page", Integer.toString(page));
+		m.add("results", Integer.toString(results));
+		m.add("sort", sort.toString());
+
+		m.add("output", "json");
+		m.add("detail", "standard");
+
+		return m;
+	}
+
+}
